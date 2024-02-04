0843ae4e61030f0a1b34ca31b502ac20e8a48016
Raymond Kennedy <franzwarning@gmail.com>
2024-02-04 16:13:08 -0500

    Auto Commit

--- app/jobs/process_file_job.rb
+++ app/jobs/process_file_job.rb
@@ -31,56 +31,11 @@ class ProcessFileJob < ApplicationJob
     file.save!
   end
 
-  # def generate_file_icon_image(file)
-  #   image_response = HTTParty.post(
-  #     "https://api.replicate.com/v1/predictions",
-  #     body: {
-  #       version: "f83b906a5a5296e7ff100ae2896f18a26e3aca546267292dc0395154fcc1cfbe",
-  #       input: {
-  #         width: 800,
-  #         height: 1024,
-  #         prompt: "A poster to sell a digital file called \"#{file.file_name}\"",
-  #         refine: "expert_ensemble_refiner",
-  #         scheduler: "K_EULER",
-  #         lora_scale: 0.6,
-  #         num_outputs: 1,
-  #         guidance_scale: 7.5,
-  #         apply_watermark: true,
-  #         high_noise_frac: 0.85,
-  #         negative_prompt: "Ugly, Bad proportion, Distorted, Cluttered, Chaotic, Mismatched, Overcrowded, Imbalanced, Inharmonious, Disproportionate, Unpleasant, Grungy, Messy, Unrefined, Crude, Grotesque, Disarray, Jumbled, Haphazard, Unsymmetrical",
-  #         prompt_strength: 0.8,
-  #         num_inference_steps: 50
-  #       }
-  #     }.to_json,
-  #     headers: {
-  #       "Authorization" => "Token #{ENV["REPLICATE_API_TOKEN"]}",
-  #       "Content-Type" => "application/json"
-  #     }
-  #   )
-  #   # image_response = openai_client.images.generate(parameters: {prompt: "A file icon vector for a \".#{file.extension}\" file. 3D art style. Smooth, elegant. DO NOT include any text. Just the iconography. For example, for a .json file use brackets instead of text. For a png, use an image placeholder instead of text. Light colorful background behind the icon, waves or gradients."})
-  #   image_url = image_response["output"][0]
-
-  #   # upload the image to supabase, first download it
-  #   downloaded_image_response = HTTParty.get(image_url)
-
-  #   uuid = SecureRandom.uuid
-  #   result = supabase_request(
-  #     path: "/storage/v1/object/gen_files/#{uuid}.png",
-  #     method: "post",
-  #     body: downloaded_image_response.body
-  #   )
-
-  #   file.display_image_url = "#{ENV["SUPABASE_URL"]}/storage/v1/object/public/#{result["Key"]}"
-  #   file.save!
-  # end
-
   def perform(user_file_id, file_size)
     print "Processing file #{user_file_id}"
     file = UserFile.find_by(id: user_file_id)
 
     t1 = Thread.new { generate_description_and_price(file, file_size) }
-    # t2 = Thread.new { generate_file_icon_image(file) }
     t1.join
-    # t2.join
   end
 end
